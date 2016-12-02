3b1b3bd2af6c42c042e9320e35855a3c2e6827b7
Sean DMR <smr48@bath.ac.uk>
2016-12-02 16:05:16 +0000

    up to ex 14 in LRtHW

---
+++ lrthw/ex14.rb
@@ -0,0 +1,26 @@
+user_name = ARGV.first #gets the first arg
+prompt = '> '
+
+puts "Hi #{user_name}."
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{user_name}."
+print prompt
+likes = $stdin.gets.chomp
+
+puts "Where do you live #{user_name}?"
+print prompt
+lives = $stdin.gets.chomp
+
+# a comma for puts is like using it twice
+
+puts "What kind of computer do you have?"
+# this was originally commad in the puts. Changed to print because the newline was weird after a prompt...
+print prompt
+
+computer = $stdin.gets.chomp
+
+puts """
+Alright so you said #{likes} about liking me.
+You live in #{lives}.
+And you have a #{computer} computer. Nice.
+"""
