895a3ba69c7bdfc69c5cfd795b57402c0c74abf8
Sean DMR <smr48@bath.ac.uk>
2017-01-12 16:25:57 +0000

    commented places-test

--- lrthw/ex43/places-test.rb
+++ lrthw/ex43/places-test.rb
@@ -2,10 +2,12 @@
 
 require './places.rb'
 
+# this works for the class...
 Map.scenes.each do |name, scene|
   puts "#{name}"
 end
 
+## but it doesn't work for objects of the class
 map = Map.new('central_corridor')
 map.scenes.each do |name, scene|
   puts "testing #{name} scene\n"
